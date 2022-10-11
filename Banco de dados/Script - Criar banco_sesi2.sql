5ab891081c9b2b628eb90346f58ca3c8c228d6bd
Matheus Felipe <matheus.rgt@hotmail.com>
2022-10-11 18:13:18 -0300

    Add files via upload

---
+++ Banco de dados/Script - Criar banco_sesi2.sql
@@ -0,0 +1,89 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: banco_sesi2
+-- ------------------------------------------------------
+-- Server version	5.6.33-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `candidato`
+--
+
+DROP TABLE IF EXISTS `candidato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `candidato` (
+  `id` bigint(20) NOT NULL DEFAULT '0',
+  `email` varchar(255) DEFAULT NULL,
+  `nome_candidato` varchar(255) DEFAULT NULL,
+  `rg` varchar(255) DEFAULT NULL,
+  `vaga_codigo` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `candidato`
+--
+
+LOCK TABLES `candidato` WRITE;
+/*!40000 ALTER TABLE `candidato` DISABLE KEYS */;
+INSERT INTO `candidato` VALUES (1,'josedascouves@senai.com.br','José das Couves','12.345.678-00','1');
+/*!40000 ALTER TABLE `candidato` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vaga`
+--
+
+DROP TABLE IF EXISTS `vaga`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vaga` (
+  `codigo` bigint(20) NOT NULL,
+  `data` varchar(255) DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `salario` decimal(19,2) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vaga`
+--
+
+LOCK TABLES `vaga` WRITE;
+/*!40000 ALTER TABLE `vaga` DISABLE KEYS */;
+INSERT INTO `vaga` VALUES (1,'31.12.2022','Vaga para instrutor de tecnologia em que o perfil do profissional deve incluir conhecimentos em banco de dados e desenvolvimento de sistemas em Java.','Instrutor de Tecnologia',2500.00);
+/*!40000 ALTER TABLE `vaga` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'banco_sesi2'
+--
+
+--
+-- Dumping routines for database 'banco_sesi2'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-11 17:37:13
